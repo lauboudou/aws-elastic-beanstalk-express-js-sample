c870e2797d226943e705867c103d2ccff1081826
lauboudou <166701668+lauboudou@users.noreply.github.com>
2025-03-11 21:19:40 +0100

    Update app.js
    updated at 11/03/2025 21h19

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello from Lauboudou updated !'));
+app.get('/', (req, res) => res.send('Hello from Lauboudou updated at 11/03/2025 21h19 !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
